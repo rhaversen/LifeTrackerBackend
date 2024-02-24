f9e4b4744089b44bac01ebd1fd38f9095276bb52
rhaversen <rhaversen@gmail.com>
2024-02-24 16:04:17 +0100

    Create track at current time, save to user with access token

--- src/controllers/tracksController.ts
+++ src/controllers/tracksController.ts
@@ -5,26 +5,30 @@ import { type NextFunction, type Request, type Response } from 'express'
 
 // Own modules
 import TrackModel, { type ITrack } from '../models/Track.js'
+import UserModel from '../models/User.js'
 
-export async function createTrack (req: Request, res: Response, next: NextFunction): Promise<void> {
+export async function createTrackAtCurrentTime (req: Request, res: Response, next: NextFunction): Promise<void> {
     interface TrackRequestBody {
+        accessToken: string
         trackName: string
-        UTCDateTime: string
     }
 
     const {
-        trackName,
-        UTCDateTime
+        accessToken,
+        trackName
     } = req.body as TrackRequestBody
-
-    const date = new Date(UTCDateTime)
 
     const newTrack = new TrackModel({
         trackName,
-        UTCDateTime: date
+        UTCDateTime: Date.now()
     })
 
     const savedTrack = await newTrack.save() as ITrack
 
+    const filter = { accessToken }
+    const update = { $push: { tracks: savedTrack } }
+    await UserModel.findOneAndUpdate(filter, update).exec()
+
     res.status(201).json(savedTrack)
 }
+
--- src/routes/tracks.ts
+++ src/routes/tracks.ts
@@ -7,18 +7,22 @@ import Router from 'express'
 import asyncErrorHandler from '../utils/asyncErrorHandler.js'
 
 // Controller functions
-import { createTrack } from '../controllers/tracksController.js'
+import {
+    createTrackAtCurrentTime
+} from '../controllers/tracksController.js'
 
 // Destructuring and global variables
 const router = Router()
 
 /**
  * @route POST api/v1/tracks
- * @desc Post a new track to the access token provided in the body
+ * @desc Post a new track at the current time to the access token provided in the body.
  * @access Public
+ * @param {string} req.body.accessToken The access token required to authenticate the request.
+ * @param {Object} req.body.name The name of the track.
  */
-router.post('/',
-    asyncErrorHandler(createTrack)
+router.post('/current-time',
+    asyncErrorHandler(createTrackAtCurrentTime)
 )
 
 export default router
