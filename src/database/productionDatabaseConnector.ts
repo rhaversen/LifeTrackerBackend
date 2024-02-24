ecff692d0bcdf923f8703c9263d42b8d0327e998
rhaversen <rhaversen@gmail.com>
2024-02-24 15:11:49 +0100

    Rework production database connector

--- src/database/productionDatabaseConnector.ts
+++ src/database/productionDatabaseConnector.ts
@@ -12,28 +12,25 @@ const {
     maxRetryAttempts,
     retryInterval
 } = config
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
 
-let currentRetryAttempt = 0
+async function connectToMongoDB (): Promise<void> {
+    for (let currentRetryAttempt = 0; currentRetryAttempt < maxRetryAttempts; currentRetryAttempt++) {
+        logger.info('Attempting connection to MongoDB')
 
-while (currentRetryAttempt < maxRetryAttempts) {
-    logger.info('Attempting connection to MongoDB')
-
-    try {
-        const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
-        // Use Mongoose to connect for production database
-        await mongoose.connect(mongoUri, mongooseOpts)
-        // Send a ping to confirm a successful connection
-        if (mongoose.connection.readyState !== 1) throw new Error()
-        logger.info('Connected to MongoDB')
-    } catch (error: any) {
-        logger.error(`Error connecting to MongoDB: ${error.message !== undefined ? error.message : error}`)
-        process.exit(1)
+        try {
+            await mongoose.connect(mongoUri, mongooseOpts)
+            logger.info('Connected to MongoDB')
+            return // Successfully connected
+        } catch (error: any) {
+            logger.error(`Error connecting to MongoDB: ${error.message !== undefined ? error.message : error}`)
+            await new Promise(resolve => setTimeout(resolve, retryInterval))
+        }
     }
-    currentRetryAttempt++
-    await new Promise((resolve) => setTimeout(resolve, retryInterval))
-}
 
-if (mongoose.connection.readyState !== 1) {
+    // Exhausted retries
     logger.error(`Failed to connect to MongoDB after ${maxRetryAttempts} attempts. Shutting down.`)
     process.exit(1)
 }
+
+await connectToMongoDB()
