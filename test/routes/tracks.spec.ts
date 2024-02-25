9ddb5881dc99452417399852471212b98bcdd118
rhaversen <rhaversen@gmail.com>
2024-02-25 15:11:11 +0100

    DRY

--- test/routes/tracks.spec.ts
+++ test/routes/tracks.spec.ts
@@ -14,16 +14,17 @@ const { expect } = chaiHttpObject
 
 describe('Post a new track', function () {
     let testUser: IUser
+    let track: { trackName: string, accessToken: string }
 
     beforeEach(async function () {
         testUser = new UserModel({
             username: 'TestUser'
         })
         await testUser.save()
+        track = { trackName: 'test', accessToken: testUser.accessToken }
     })
 
     it('should create a track', async function () {
-        const track = { trackName: 'test', accessToken: testUser.accessToken }
         await agent.post('/v1/tracks').send(track)
         const allTracks = await TrackModel.find({}).exec()
         expect(allTracks.length).to.equal(1)
@@ -31,19 +32,16 @@ describe('Post a new track', function () {
     })
 
     it('should respond with status code 201', async function () {
-        const track = { trackName: 'test', accessToken: testUser.accessToken }
         const res = await agent.post('/v1/tracks').send(track)
         expect(res).to.have.status(201)
     })
 
     it('should respond with the track', async function () {
-        const track = { trackName: 'test', accessToken: testUser.accessToken }
         const res = await agent.post('/v1/tracks').send(track)
         expect(res.body.trackName).to.equal(track.trackName)
     })
 
     it('should add the track to the user', async function () {
-        const track = { trackName: 'test', accessToken: testUser.accessToken }
         await agent.post('/v1/tracks').send(track)
         const foundUser = await UserModel.findOne({}).exec() as IUser
         const foundTrack = await TrackModel.findOne({}).exec() as ITrack
