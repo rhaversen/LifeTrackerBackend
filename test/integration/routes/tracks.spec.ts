745d1c12d5bfa45068fdcba247b6fbf00d9a61b9
rhaversen <rhaversen@gmail.com>
2024-03-05 16:49:50 +0100

    Delete last track integration tests

--- test/integration/routes/tracks.spec.ts
+++ test/integration/routes/tracks.spec.ts
@@ -5,6 +5,7 @@
 // Third-party libraries
 import sinon from 'sinon'
 import { expect } from 'chai'
+import { type Response } from 'superagent'
 
 // Own modules
 import { chaiAppServer as agent } from '../../testSetup.js'
@@ -204,6 +205,288 @@ describe('POST api/v1/tracks', function () {
         it('should respond with status code 400', async function () {
             const res = await agent.post('/v1/tracks').send(track)
             expect(res).to.have.status(400)
+        })
+    })
+})
+
+describe('DELETE api/v1/tracks/last', function () {
+    let testUserA: IUser
+    let testTrackA1: ITrack
+
+    beforeEach(async function () {
+        testUserA = new UserModel({
+            userName: 'testUserA',
+            signUpDate: new Date()
+        })
+        await testUserA.save()
+        testTrackA1 = new TrackModel({
+            trackName: 'testTrackA1',
+            date: new Date(),
+            userId: testUserA._id
+        })
+        await testTrackA1.save()
+    })
+
+    describe('Delete a track', function () {
+        let res: Response
+
+        beforeEach(async function () {
+            res = await agent.delete('/v1/tracks/last').send({ accessToken: testUserA.accessToken })
+        })
+
+        it('should delete a track', async function () {
+            const allTracks = await TrackModel.find({}).exec()
+            expect(allTracks.length).to.equal(0)
+        })
+
+        it('should respond with status code 204', async function () {
+            expect(res).to.have.status(204)
+        })
+
+        it('should have an empty body', async function () {
+            // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+            expect(res.body).to.be.empty
+        })
+    })
+
+    describe('Delete last created track with earlier date (timeOffset)', function () {
+        let earlierDateTrack: ITrack
+
+        beforeEach(async function () {
+            earlierDateTrack = new TrackModel({
+                trackName: 'earlierDateTrack',
+                date: new Date(Date.now() - 1000 * 60 * 60 * 24 * 7), // A week ago
+                userId: testUserA._id
+            })
+            await earlierDateTrack.save()
+            await agent.delete('/v1/tracks/last').send({ accessToken: testUserA.accessToken })
+        })
+
+        it('should delete the newest created track', async function () {
+            const allTracks = await TrackModel.find({}).exec()
+            expect(allTracks[0].trackName).to.equal('testTrackA1')
+        })
+    })
+
+    describe('Invalid accessToken', function () {
+        let res: Response
+
+        beforeEach(async function () {
+            res = await agent.delete('/v1/tracks/last').send({ accessToken: 'invalidAccessToken' })
+        })
+
+        it('should not delete track', async function () {
+            const allTracks = await TrackModel.find({}).exec()
+            expect(allTracks.length).to.equal(1)
+        })
+
+        it('should respond with status code 400', async function () {
+            expect(res).to.have.status(400)
+        })
+    })
+
+    describe('No accessToken', function () {
+        let res: Response
+
+        beforeEach(async function () {
+            res = await agent.delete('/v1/tracks/last').send({})
+        })
+
+        it('should not delete track', async function () {
+            const allTracks = await TrackModel.find({}).exec()
+            expect(allTracks.length).to.equal(1)
+        })
+
+        it('should respond with status code 400', async function () {
+            expect(res).to.have.status(400)
+        })
+    })
+
+    describe('Empty accessToken', function () {
+        let res: Response
+
+        beforeEach(async function () {
+            res = await agent.delete('/v1/tracks/last').send({ accessToken: '' })
+        })
+
+        it('should not delete track', async function () {
+            const allTracks = await TrackModel.find({}).exec()
+            expect(allTracks.length).to.equal(1)
+        })
+
+        it('should respond with status code 400', async function () {
+            expect(res).to.have.status(400)
+        })
+    })
+
+    describe('Multiple tracks', function () {
+        beforeEach(async function () {
+            const newTrack = new TrackModel({
+                trackName: 'newTrack',
+                date: new Date(),
+                userId: testUserA._id
+            })
+            await newTrack.save()
+            await agent.delete('/v1/tracks/last').send({ accessToken: testUserA.accessToken })
+        })
+
+        it('should delete a single track', async function () {
+            const allTracks = await TrackModel.find({}).exec()
+            expect(allTracks.length).to.equal(1)
+        })
+
+        it('should delete last track', async function () {
+            const allTracks = await TrackModel.find({}).exec()
+            expect(allTracks[0].trackName).to.equal('testTrackA1')
+        })
+
+        it('should respond with status code 204', async function () {
+            const res = await agent.delete('/v1/tracks/last').send({ accessToken: testUserA.accessToken })
+            expect(res).to.have.status(204)
+        })
+
+        it('should have an empty body', async function () {
+            const res = await agent.delete('/v1/tracks/last').send({ accessToken: testUserA.accessToken })
+            // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+            expect(res.body).to.be.empty
+        })
+    })
+
+    describe('No tracks', function () {
+        let res: Response
+
+        beforeEach(async function () {
+            await TrackModel.deleteMany({}).exec()
+            res = await agent.delete('/v1/tracks/last').send({ accessToken: testUserA.accessToken })
+        })
+
+        it('should not delete a track', async function () {
+            const allTracks = await TrackModel.find({}).exec()
+            expect(allTracks.length).to.equal(0)
+        })
+
+        it('should respond with status code 204', async function () {
+            expect(res).to.have.status(204)
+        })
+
+        it('should have an empty body', async function () {
+            // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+            expect(res.body).to.be.empty
+        })
+    })
+
+    describe('Multiple users', function () {
+        let testUserB: IUser
+        let testTrackB1: ITrack
+
+        beforeEach(async function () {
+            testUserB = new UserModel({
+                userName: 'testUserB',
+                signUpDate: new Date()
+            })
+            await testUserB.save()
+            testTrackB1 = new TrackModel({
+                trackName: 'testTrackB1',
+                date: new Date(),
+                userId: testUserB._id
+            })
+            await testTrackB1.save()
+        })
+
+        describe('No tracks', function () {
+            let res: Response
+
+            beforeEach(async function () {
+                await TrackModel.deleteMany({}).exec()
+                res = await agent.delete('/v1/tracks/last').send({ accessToken: testUserA.accessToken })
+            })
+
+            it('should not delete a track', async function () {
+                const allTracks = await TrackModel.find({}).exec()
+                expect(allTracks.length).to.equal(0)
+            })
+
+            it('should respond with status code 204', async function () {
+                expect(res).to.have.status(204)
+            })
+
+            it('should have an empty body', async function () {
+                // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+                expect(res.body).to.be.empty
+            })
+        })
+
+        describe('Single track', function () {
+            let res: Response
+
+            beforeEach(async function () {
+                res = await agent.delete('/v1/tracks/last').send({ accessToken: testUserA.accessToken })
+            })
+
+            it('should delete a track', async function () {
+                const testUserATracks = await TrackModel.find({ userId: testUserA._id }).exec()
+                expect(testUserATracks.length).to.equal(0)
+            })
+
+            it('should not delete any tracks of the other user', async function () {
+                const testUserBTracks = await TrackModel.find({ userId: testUserB._id }).exec()
+                expect(testUserBTracks.length).to.equal(1)
+            })
+
+            it('should respond with status code 204', async function () {
+                expect(res).to.have.status(204)
+            })
+
+            it('should have an empty body', async function () {
+                // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+                expect(res.body).to.be.empty
+            })
+        })
+
+        describe('Multiple tracks', function () {
+            let testTrackA2: ITrack
+            let testTrackB2: ITrack
+            let res: Response
+
+            beforeEach(async function () {
+                testTrackA2 = new TrackModel({
+                    trackName: 'testTrackA2',
+                    date: new Date(),
+                    userId: testUserA._id
+                })
+                await testTrackA2.save()
+                testTrackB2 = new TrackModel({
+                    trackName: 'testTrackB2',
+                    date: new Date(),
+                    userId: testUserB._id
+                })
+                await testTrackB2.save()
+                res = await agent.delete('/v1/tracks/last').send({ accessToken: testUserA.accessToken })
+            })
+
+            it('should delete a single track', async function () {
+                const testUserATracks = await TrackModel.find({ userId: testUserA._id }).exec()
+                expect(testUserATracks.length).to.equal(1)
+            })
+
+            it('should delete the last track', async function () {
+                const testUserATracks = await TrackModel.find({ userId: testUserA._id }).exec()
+                expect(testUserATracks[0].trackName).to.equal('testTrackA1')
+            })
+
+            it('should not delete any tracks of the other user', async function () {
+                const testUserBTracks = await TrackModel.find({ userId: testUserB._id }).exec()
+                expect(testUserBTracks.length).to.equal(2)
+            })
+
+            it('should respond with status code 204', async function () {
+                expect(res).to.have.status(204)
+            })
+
+            it('should have an empty body', async function () {
+                // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+                expect(res.body).to.be.empty
+            })
         })
     })
 })
