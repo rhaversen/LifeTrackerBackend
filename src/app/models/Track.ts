94d83047bb61a6905bdb17ebaa271ae125da35e8
rhaversen <rhaversen@gmail.com>
2024-04-03 03:55:24 +0200

    Introduce enums for track types

--- src/app/models/Track.ts
+++ src/app/models/Track.ts
@@ -5,27 +5,49 @@ import { type Document, model, Schema, type Types } from 'mongoose'
 
 // Own modules
 import logger from '../utils/logger.js'
+import { trackTypes } from '../utils/trackTypes.js'
 
 // Destructuring and global variables
+
+if (process.env.NODE_ENV === 'test') {
+    // Adding test track types to trackTypes
+    const testTrackTypes = await import('../../test/testTrackTypes.js')
+    Object.assign(trackTypes, testTrackTypes.testTrackTypes)
+}
+
+// Freezing the trackType object
+Object.freeze(trackTypes)
 
 export interface ITrack extends Document {
     // Properties
     _id: Types.ObjectId
     trackName: string
     date: Date // The date the track took place
+    duration?: number // The duration of the track in minutes
     userId: Types.ObjectId // The user who created the track
     createdAt: Date // The date the track was created in the system
+    data?: Record<string, unknown> // The data of the track
 }
 
 const trackSchema = new Schema<ITrack>({
     trackName: {
-        type: String,
-        required: true
+        type: Schema.Types.String,
+        required: true,
+        enum: Object.keys(trackTypes)
     },
     date: {
-        type: Date,
+        type: Schema.Types.Date,
         required: true,
         default: Date.now
+    },
+    duration: {
+        type: Number,
+        required: false,
+        default: 0,
+        validate: {
+            validator: (value: number) => value >= 0,
+            message: 'Duration must be a positive number'
+        }
     },
     userId: {
         type: Schema.Types.ObjectId,
@@ -36,6 +58,10 @@ const trackSchema = new Schema<ITrack>({
         type: Date,
         required: true,
         default: Date.now
+    },
+    data: {
+        type: Schema.Types.Mixed,
+        required: false
     }
 })
 
@@ -49,6 +75,28 @@ trackSchema.pre('save', function (next) {
     logger.silly('Saving track')
     next()
 })
+
+// Adding validation to data
+trackSchema.path('data').validate(function (value) {
+    // Check if the trackName is a valid track type
+    if (!Object.keys(trackTypes).includes(this.trackName)) return false
+
+    // No data is always valid data
+    if (this.data === undefined || this.data === null) return true
+
+    // Get the allowed keys for the track type
+    const allowedKeys = trackTypes[this.trackName as keyof typeof trackTypes].allowedKeys as Record<string, unknown> // The type assertion is necessary because TypeScript does not know that the trackName is a valid key of trackType
+
+    // Check if the data has the allowed keys and the correct types
+    for (const key in this.data) {
+        // Check if key is allowed
+        if (!Object.keys(allowedKeys).includes(key)) return false
+        // Check if the type is correct
+        if (typeof this.data[key] !== typeof allowedKeys[key]) return false
+    }
+
+    return true
+}, 'Data is not valid')
 
 // Compile the schema into a model
 const TrackModel = model<ITrack>('Track', trackSchema)
---
+++ src/app/utils/trackTypes.ts
@@ -0,0 +1,100 @@
+export const trackTypes = {
+    TEST_TRACK: {
+        allowedKeys: {
+        }
+    },
+    CONSUMED_WATER: {
+        allowedKeys: {
+            litresProduct: Number
+        }
+    },
+    CONSUMED_ALCOHOL: {
+        allowedKeys: {
+            litresProduct: Number,
+            alcoholPercentage: Number
+        }
+    },
+    CONSUMED_FOOD: {
+        allowedKeys: {
+            gramsProduct: Number,
+            caloriesTotal: Number
+        }
+    },
+    CONSUMED_CIGARETTE: {
+        allowedKeys: {
+            gramsProduct: Number,
+            gramsNicotine: Number
+        }
+    },
+    CONSUMED_SNUFF: {
+        allowedKeys: {
+            gramsProduct: Number,
+            gramsNicotine: Number
+        }
+    },
+    EXCRETED_URINE: {
+        allowedKeys: {
+            litres: Number
+        }
+    },
+    EXCRETED_FECES: {
+        allowedKeys: {
+            litres: Number
+        }
+    },
+    EXCRETED_VOMIT: {
+        allowedKeys: {
+            litres: Number
+        }
+    },
+    EXCRETED_SEMEN: {
+        allowedKeys: {
+            litres: Number
+        }
+    },
+
+    BLOW_NOSE: {
+        allowedKeys: {
+        }
+    },
+    BRUSH_TEETH: {
+        allowedKeys: {
+        }
+    },
+    SHOWER: {
+        allowedKeys: {
+        }
+    },
+    SHAVE: {
+        allowedKeys: {
+        }
+    },
+    HEARTACHE: {
+        allowedKeys: {
+        }
+    },
+    HEADACHE: {
+        allowedKeys: {
+        }
+    },
+    MASTURBATE: {
+        allowedKeys: {
+        }
+    },
+    SEX: {
+        allowedKeys: {
+        }
+    },
+    CLIP_NAILS: {
+        allowedKeys: {
+        }
+    },
+    COOKING: {
+        allowedKeys: {
+        }
+    },
+    CLEANING: {
+        allowedKeys: {
+        }
+    }
+}
