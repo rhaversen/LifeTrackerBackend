90c2c7f1fac6a77a63ca7930ede1a5493a3adac2
rhaversen <rhaversen@gmail.com>
2024-04-16 04:45:59 +0200

    Deprecate healthcheck route, use livez and readyz for monitoring

--- src/app/routes/service.ts
+++ src/app/routes/service.ts
@@ -1,44 +1,13 @@
 // Node.js built-in modules
 
 // Third-party libraries
-import { type NextFunction, type Request, type Response, Router } from 'express'
+import { Router } from 'express'
 import mongoose from 'mongoose'
 
 // Own modules
-import asyncErrorHandler from '../utils/asyncErrorHandler.js'
-
-// Controller functions
 
 // Destructuring and global variables
 const router = Router()
-
-// Functions
-async function checkHealth (req: Request, res: Response, next: NextFunction): Promise<void> {
-    const uptimeSeconds = process.uptime()
-    const uptimeHours = uptimeSeconds / 60 / 60
-    const uptimeDays = uptimeHours / 24
-    const uptimeWeeks = uptimeDays / 7
-    const uptimeMonths = uptimeDays / 30
-
-    const customHealthInfo = {
-        databaseConnected: mongoose.connection.readyState === 1,
-        timestamp: new Date().toISOString(), // Current time for the health check
-        uptime: {
-            seconds: uptimeSeconds,
-            hours: uptimeHours,
-            days: uptimeDays,
-            weeks: uptimeWeeks,
-            months: uptimeMonths
-        }
-    }
-
-    res.status(200).json(customHealthInfo)
-}
-
-// Routes
-router.get('/healthcheck',
-    asyncErrorHandler(checkHealth)
-)
 
 router.get('/livez', (req, res) => {
     res.status(200).send('OK')
